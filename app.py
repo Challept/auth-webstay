bdec3f1a9ff305fa6b7fda30f3449a225645d932
Notch3w <119951713+Challept@users.noreply.github.com>
2025-01-06 19:37:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -123,5 +123,6 @@ def google_callback():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(host="0.0.0.0", port=5000, debug=True)
